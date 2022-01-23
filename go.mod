0a64ed8818ebac76b8a4a5f151e0e816a6c79df6
raihaninfo <raihanmahmudi35@gmail.com>
2022-01-23 23:13:56 +0600

    create database connection

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/raihaninfo/couchbase
+
+go 1.17
+
+require (
+	github.com/elliotchance/orderedmap v1.4.0 // indirect
+	github.com/mateors/mcb v1.0.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+
+	"github.com/mateors/mcb"
+)
+
+var db *mcb.DB
+
+type myTable struct {
+	Name       string   `json:"name"`
+	Age        int      `json:"age"`
+	Profession string   `json:"profession"`
+	Hobbies    []string `json:"hobbies"`
+	Type       string   `json:"type"`
+}
+
+func init() {
+
+	db = mcb.Connect(host, username, password, false)
+
+	res, err := db.Ping()
+	if err != nil {
+
+		fmt.Println(res)
+		os.Exit(1)
+	}
+	fmt.Println(res, err)
+
+}
+
+func main() {
+
+	//How to insert into couchbase bucket
+	var myData myTable
+
+	form := make(url.Values, 0)
+	form.Add("bucket", "bucketName") //bucket and collection-> namespace:bucket.scope.collection
+	form.Add("aid", "d006")          //document ID
+	form.Add("name", "Mostain Billah")
+	form.Add("age", "36")
+	form.Add("profession", "Developer")
+	form.Add("hobbies", "Programming")
+	form.Add("hobbies", "Problem Solving")
+	form.Add("type", "participant") //what type of data or table name in general (SQL)
+
+	p := db.Insert(form, &myData)    //pass by reference (&myData)
+	fmt.Println("Status:", p.Status) //p.Status == Success means data successfully inserted to bucket.
+
+}
